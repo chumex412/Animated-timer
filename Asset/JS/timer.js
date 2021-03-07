53fb34eddca921541b19be9f4cdb948e46a901b5
chumex412 <64347090+chumex412@users.noreply.github.com>
2021-03-07 09:30:12 +0100

    First commit added index.html, Assets with CSS and JS with some basic css reset

---
+++ Asset/JS/timer.js
@@ -0,0 +1,15 @@
+class Timer {
+  constructor(durationInput, startBtn, stopBtn) {
+    this.durationInput = durationInput;
+    this.startBtn = startBtn;
+    this.stopBtn = stopBtn;
+  }
+
+  start() {
+
+  }
+
+  stop() {
+    
+  }
+}
